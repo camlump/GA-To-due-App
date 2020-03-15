4f9f8e2e09b64814bced36310a14069724b7835a
CameronLumpkinATL <camlump@yahoo.com>
2020-03-14 23:51:05 -0400

    created api call and functions for getting a single project

--- client/src/components/SingleProject.jsx
+++ client/src/components/SingleProject.jsx
@@ -1,6 +1,55 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { response } from 'express';
 
 export default class SingleProject extends Component {
+    state = {
+        project: {},
+        redirectToProjects: false,
+        editProject: {}
+    }
+    getProject = () =>{
+        const projectId = this.props.match.params.projectId;
+        axios.get('api/project' + projectId).then((response)=>{
+                this.setState({
+                    project: response.data,
+                    editProject: response.data
+                });
+        });
+    }
+    changeInput = (event) =>{
+        const editedProject = {...this.state.editProject}
+        editedProject[event.target.name] = event.target.value
+            this.setState({
+                editProject: editedProject
+            })
+    }
+
+    submitEditForm = (event) => {
+        event.preventDefault();
+        const projectId = this.props.match.params.projectId;
+        axios.delete('/api/project' + projectId, this.state.projectId).then(()=>{
+            this.setState({
+                redirectToProjects: true
+            });
+        });
+        
+    }
+
+    deleteProject = () =>{
+        
+        const projectId = this.props.match.params.projectId;
+        axios.delete('/api/project' + projectId).then(()=>{
+            this.setState({
+                redirectToProjects: true
+            })
+        })
+    }
+
+    componentDidMount(){
+        this.getProject()
+    }
+    
     render() {
         return (
             <div>
