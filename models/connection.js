3c298f463a16c1c3a6712f5bcb7768fbf6fecd5d
CameronLumpkinATL <camlump@yahoo.com>
2021-02-22 14:54:14 -0500

    updated mongoose connection

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,7 @@
 /* Import the mongoose module
  *
  */
+require("dotenv").config()
 // MONGO ATLAS CONNECTION STRING
 // mongodb+srv://heroku_pbb3c6f6:<password>@cluster-pbb3c6f6.5hxov.mongodb.net/heroku_pbb3c6f6?retryWrites=true&w=majority
 const mongoose = require('mongoose');
@@ -10,8 +11,9 @@ const mongoose = require('mongoose');
  * TODO: replace <db-name> with the name of your mongo database. 
  * This will need to change for every new project you create.
  *
- */
-const connectionString = process.env.MONGODB_URI || "mongodb+srv://Cam:gaduelist@cluster0.hsthf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//  */
+// const connectionString = process.env.MONGODB_URI || "";
+const mongoURI = process.env.Mongo_URI;
 
 
 /* Step 2
@@ -20,10 +22,17 @@ const connectionString = process.env.MONGODB_URI || "mongodb+srv://Cam:gaduelist
  *
  * NOTE: newUrlParser diables a deprecation warning
  */
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
-  });
+// mongoose.connect(connectionString, { useNewUrlParser: true})
+//   .then(() => {
+//     console.log("connected to mongo at: " + connectionString);
+//   });
+
+mongoose.connect(mongoURI, {useNewUrlParser:true }, (error)=>{
+  if(error) {
+      return console.log(error)
+  }
+  return console.log("database is connected")
+})
 
 
 /* Export the mongoose object.
